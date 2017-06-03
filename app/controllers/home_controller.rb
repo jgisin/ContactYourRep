f0b0fe806d4cbc18589477a57657f9494df0d4aa
CHAD STACHOWICZ <chadstachowicz@chads-imac.attlocal.net>
2017-06-03 01:56:28 -0400

    First commit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,4 +8,35 @@ class HomeController < ApplicationController
   def index
 
   end
+
+  def reps
+    address = params[:address] + " " + params[:city] + " " + params[:state] + " " + params[:zip]
+    offices = Array.new
+    @persons = Array.new
+    officials = Array.new
+    @response = HTTParty.get("https://www.googleapis.com/civicinfo/v2/representatives?key=AIzaSyCE4kP016XrAmixkE9iR7gHjOGJ9YeaX8Y&&levels=country&levels=administrativeArea1&address=" + address)
+    @response["offices"].each do |off|
+      offices << off
+    end
+    @response["officials"].each do |offic|
+      officials << offic
+    end
+    offices.each do |o|
+      o["officialIndices"].each do |i,ind|
+        person = Hash.new
+        person["office"] = o["name"]
+        person["name"] = officials[i]["name"]
+        person["address"] = officials[i]["address"]
+        person["photo"] = officials[i]["photoUrl"]
+        person["party"] = officials[i]["party"]
+        person["phone"] = officials[i]["phones"][0]
+        officials[i]["channels"].each do |chan|
+           if chan["type"] == "Twitter"
+            person["twitter"] = chan["id"]
+          end
+        end
+        @persons << person
+      end
+    end
+  end
 end
